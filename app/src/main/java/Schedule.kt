eca3dd1f6825d41d49dafda5d847131d0e64eb29
Jason Lee <jasonepic0801@gmail.com>
2018-07-16 00:08:37 +0800

    Initial commit. Woohoo, let's go!

---
+++ app/src/main/java/Schedule.kt
@@ -0,0 +1,12 @@
+package comdiocleiagenda_timetable.github.agenda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Schedule : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
